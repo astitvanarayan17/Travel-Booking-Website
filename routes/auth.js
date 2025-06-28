b8ffb8d837b5e360b88e0f62d41d0cf84837aa6f
astitva <astitvanarayan17@gmail.com>
2025-06-28 10:42:59 +0530

    Travel Booking Website Project

--- routes/auth.js
+++ routes/auth.js
@@ -3,25 +3,29 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
-router.get('/register', (req, res) => res.render('register'));
+router.get('/register', (req, res) => res.render('register', { error: null }));
 
 router.post('/register', async (req, res) => {
     const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+        return res.render('register', { error: 'Username already exists' });
+    }
     const hashed = await bcrypt.hash(password, 10);
     await new User({ username, password: hashed }).save();
     res.redirect('/auth/login');
 });
 
-router.get('/login', (req, res) => res.render('login'));
+router.get('/login', (req, res) => res.render('login', { error: null }));
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    if(user && await bcrypt.compare(password, user.password)){
+    if (user && await bcrypt.compare(password, user.password)) {
         req.session.userId = user._id;
         res.redirect('/');
     } else {
-        res.send('Invalid credentials');
+        res.render('login', { error: 'Invalid username or password' });
     }
 });
 
